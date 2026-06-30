b36b853e2b3177e0239fe6f6de581e986355f3a0
Salty <mavril763@gmail.com>
2026-06-30 16:08:47 -0400

    Finished settings icons controller (?)

--- SOControllers/IconReplacement/SOSystemSettingsIconReplacementPageController.m
+++ SOControllers/IconReplacement/SOSystemSettingsIconReplacementPageController.m
@@ -75,25 +75,39 @@
         sender.image = nsImg;
     }
     
+    const SOEncodedKeyPath tPrefKey = {
+        .rootKey = &kSOIconsExtensionDict,
+        .components = @[selectedValueOrNil]
+    };
+    
     if (sender.image){
         [self.mutableDict setObject:filename forKey:selectedValueOrNil];
+        
+        [self setPendingIconResourceChangeForKeypath:&tPrefKey
+                                            resource:[NSData dataWithContentsOfURL:sender.draggedFileURL]
+                                            filename:filename
+                                                note:[NSString stringWithFormat:@"Set %@ to %@",
+                                                      selectedValueOrNil, filename]];
         
         return;
     }
     
     [self.mutableDict removeObjectForKey:selectedValueOrNil];
+    
+    [self setPendingIconResourceChangeForKeypath:&tPrefKey
+                                        resource:nil
+                                        filename:nil
+                                            note:[NSString stringWithFormat:@"Cleared %@",
+                                                  selectedValueOrNil]];
 }
 
 - (void)objectWithKey:(id)aKey didGetSetTo:(id)anObject { 
-    
 }
 
-- (void)willRemoveObject:(id)anObject forKey:(id)aKey { 
-    
+- (void)willRemoveObject:(id)anObject forKey:(id)aKey {
 }
 
 - (void)didRemoveObjectWithKey:(id)aKey{
-    
 }
 
 @end
