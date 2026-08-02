fd970328fe805b14f08aecebda3060a5f58111ec
Salty <mavril763@gmail.com>
2026-08-02 13:37:37 -0400

    Outline view updated to show packs

--- SaltySweets/SONavigatorPane.h
+++ SaltySweets/SONavigatorPane.h
@@ -1,14 +1,19 @@
 //Created by Salty on 2/2/26.
 
 #import <AppKit/AppKit.h>
-#import <QuartzCore/QuartzCore.h>
 
-#import "SOViewPane.h"
-#import "SONavigatorBar.h"
+#import "Services/SOAtomicAccessPoint.h"
 
-@interface SONavigatorPane : NSViewController <NSSplitViewDelegate, NSOutlineViewDelegate, NSOutlineViewDataSource>
-@property (strong) IBOutlet NSSplitView *contentSplitView;
-@property (strong) IBOutlet NSOutlineView *submenuChooser;
-@property (assign) IBOutlet SOViewPane *viewPaneController;
-@property (strong) IBOutlet SONavigatorBar *navBarController;
+@interface SONavigatorPane : NSViewController <NSOutlineViewDataSource>
+@property (weak, nonatomic) IBOutlet NSOutlineView *outlineView;
+@end
+
+@interface SONavigatorPaneObject : NSObject
+@property (strong) NSString *title;
+@property (assign) BOOL containsChildren;
+@property (strong) NSMutableArray<SONavigatorPaneObject *> *children;
+
+@property (strong) SONSBundle *bundle;
+
+- (instancetype)initWithTitle:(NSString *)title bundle:(SONSBundle *)bundle containsChildren:(BOOL)containsChildren;
 @end
--- SaltySweets/SONavigatorPane.m
+++ SaltySweets/SONavigatorPane.m
@@ -2,31 +2,134 @@
 
 #import "SONavigatorPane.h"
 
-@interface SONavigatorPane () <NSOutlineViewDataSource, NSOutlineViewDelegate>
+@implementation SONavigatorPaneObject
+- (instancetype)initWithTitle:(NSString *)title
+                       bundle:(SONSBundle *)bundle
+             containsChildren:(BOOL)containsChildren{
+    self = [super init];
+    if (self){
+        _title = title;
+        _bundle = bundle;
+        _containsChildren = containsChildren;
+        _children = [NSMutableArray array];
+    }
+    return self;
+}
+@end
+
+@interface SONavigatorPane ()
+@property (strong) NSMutableArray<SONavigatorPaneObject *> *collectionArray;
 @end
 
 @implementation SONavigatorPane
-#pragma mark - NSOutlineViewDataSource Methods
+- (void)awakeFromNib{
+    [super awakeFromNib];
+    self.collectionArray = [NSMutableArray array];
+    
+    NSArray *dockItems = [self dockThemeInventory];
+    
+    SONavigatorPaneObject *dockObjects = [[SONavigatorPaneObject alloc] initWithTitle:@"Dock Themes"
+                                                                               bundle:nil
+                                                                     containsChildren:dockItems.count > 0];
+    
+    for (SONavigatorPaneObject *obj in dockItems){
+        obj.title = [obj.title stringByDeletingPathExtension];
+        [dockObjects.children addObject:obj];
+    }
+    
+    NSArray *iconItems = [self iconPackInventory];
+    
+    SONavigatorPaneObject *iconObjects = [[SONavigatorPaneObject alloc] initWithTitle:@"Icon Packs"
+                                                                               bundle:nil
+                                                                     containsChildren:iconItems.count > 0];
+    
+    for (SONavigatorPaneObject *obj in iconItems){
+        obj.title = [obj.title stringByDeletingPathExtension];
+        [iconObjects.children addObject:obj];
+    }
+    
+    [self.collectionArray addObject:dockObjects];
+    [self.collectionArray addObject:iconObjects];
 
-- (NSInteger)outlineView:(NSOutlineView *)outlineView numberOfChildrenOfItem:(id)item {
-    return 0;
+    [self.outlineView reloadData];
+}
+- (id)outlineView:(NSOutlineView *)outlineView
+            child:(NSInteger)index
+           ofItem:(SONavigatorPaneObject *)item{
+    if (!item)
+        return self.collectionArray[index];
+    
+    return [item.children objectAtIndex:index];
 }
 
-- (id)outlineView:(NSOutlineView *)outlineView child:(NSInteger)index ofItem:(id)item {
-    return nil;
+- (BOOL)outlineView:(NSOutlineView *)outlineView
+   isItemExpandable:(SONavigatorPaneObject *)item{
+    if (!item)
+        return YES;
+    
+    return item.containsChildren;
 }
 
-- (BOOL)outlineView:(NSOutlineView *)outlineView isItemExpandable:(id)item {
-    return NO;
+- (NSInteger)outlineView:(NSOutlineView *)outlineView
+  numberOfChildrenOfItem:(SONavigatorPaneObject *)item{
+    if (!item && self.collectionArray)
+        return [self.collectionArray count];
+    else if (!item)
+        return 0;
+
+    return [item.children count];
 }
 
-- (id)outlineView:(NSOutlineView *)outlineView objectValueForTableColumn:(NSTableColumn *)tableColumn byItem:(id)item {
-    return nil;
+- (id)outlineView:(NSOutlineView *)outlineView
+objectValueForTableColumn:(NSTableColumn *)tableColumn
+           byItem:(SONavigatorPaneObject *)item{
+    if (!item)
+        return self.collectionArray;
+    
+    return item.title;
 }
 
-#pragma mark - NSOutlineViewDelegate
+- (NSArray<SONavigatorPaneObject *> *)dockThemeInventory{
+    NSArray<NSURL *> *dockThemeURLs = [self URLsForDirectoryPath:[SOAtomicAccessPoint sharedInstance].dockThemeBundleDirectory];
+    
+    NSMutableArray<SONavigatorPaneObject *> *retArray = [NSMutableArray array];
+    
+    for (NSURL *url in dockThemeURLs){
+        SONavigatorPaneObject *retObject = [[SONavigatorPaneObject alloc] initWithTitle:url.lastPathComponent
+                                                                                 bundle:[[SODockThemeBundle alloc] initWithURL:url]
+                                                                       containsChildren:NO];
+        [retArray addObject:retObject];
+    }
+    
+    return retArray;
+}
 
-- (BOOL)outlineView:(NSOutlineView *)outlineView shouldSelectItem:(id)item {
-    return NO;
+- (NSArray<SONavigatorPaneObject *> *)iconPackInventory{
+    NSArray<NSURL *> *iconPackURLs = [self URLsForDirectoryPath:[SOAtomicAccessPoint sharedInstance].iconPackBundleDirectory];
+    
+    NSMutableArray<SONavigatorPaneObject *> *retArray = [NSMutableArray array];
+    
+    for (NSURL *url in iconPackURLs){
+        SONavigatorPaneObject *retObject = [[SONavigatorPaneObject alloc] initWithTitle:url.lastPathComponent
+                                                                                 bundle:[[SOSiconPackBundle alloc] initWithURL:url]
+                                                                       containsChildren:NO];
+        [retArray addObject:retObject];
+    }
+    
+    return retArray;
+}
+
+- (NSArray<NSURL *> *)URLsForDirectoryPath:(NSString *)path{
+    NSFileManager *fm = [NSFileManager defaultManager];
+    
+    NSURL *directoryURL = [NSURL fileURLWithPath:path
+                                     isDirectory:YES];
+    
+    NSArray<NSURL *> *ret = [fm contentsOfDirectoryAtURL:directoryURL
+                              includingPropertiesForKeys:nil
+                                                 options:NSDirectoryEnumerationSkipsHiddenFiles
+                                                   error:nil];
+    
+    return ret;
 }
 @end
